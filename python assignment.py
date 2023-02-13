bf2aa22cbbea36c5082b4306f414969dccc07237
Ashish Chouhan <ashishchouhan2000.11@gmail.com>
2023-02-13 12:32:01 +0530

    assignment file updated

--- python assignment.py
+++ python assignment.py
@@ -1,35 +1,33 @@
 class Board:
 
     # FUNCTION TO INITIALIZE NUMBER OF ROWS AND COLUMNS
-    def __init__(self):
+    def __init__(self, rows, cols):
 
         self.matrix = []
-        self.rows = int(input("Enter number of rows: "))
-        self.cols = int(input("Enter number of columns: "))
-        
+
+        if rows <= 0:
+            raise Exception("Invalid rows")
+
+        if cols <= 0:
+            raise Exception("Invalid columns")
+
+        self.rows = rows
+        self.cols = cols
+
+    # FUNCTION TO VALIDATE NUMBER OF ROWS
+    def validate_matrix(self):
+        if len(self.matrix) > self.rows:
+            raise Exception("Maximum number of rows exceeded")
+
+        if len(self.matrix[0]) > self.cols:
+            raise Exception("Maximum number of columns exceeded")
 
     # FUNCTION TO ENTER CORRECT VALUES IN THE MATRIX
-    def make(self):
-
-        for row in range(self.rows):
-
-            temp = []
-            
-            for col in range(self.cols):
-
-                # RUN AN INFINTE LOOP UNTIL CORRECT VALUE IS ENTERED
-                while True:
-                    
-                    val = input(f"Enter the value for row {row} and col {col}: ")
-                    if(val == "O" or val == "X"):
-                        temp.append(val)
-                        break
-                    else:
-                        print("Not a valid value")
-                        
-            self.matrix.append(temp)
-            print()
-            
+    def check(self):
+        for i in self.matrix:
+            for j in i:
+                if j != 'X' and j != 'O':
+                    raise Exception("Invalid input")
 
     # FUNCTION TO DISPLAY THE MATRIX
     def show(self):
@@ -38,106 +36,92 @@ class Board:
             print(row)
         print()
 
-        
     # FUNCTION TO SOLVE THE MATRIX
     def solve(self):
-        
+
         # TRAVERSING THE FIRST COLUMN
         for row in range(self.rows):
-    
-            self.dfs(row,0)
+            self.dfs(row, 0)
 
         # TRAVERSING THE LAST COLUMN
         for row in range(self.rows):
-    
-            self.dfs(row,self.cols-1)
+            self.dfs(row, self.cols - 1)
 
         # TRAVERSING THE FIRST ROW
         for col in range(self.cols):
-    
-            self.dfs(0,col)
+            self.dfs(0, col)
 
         # TRAVERSING THE LAST ROW
         for col in range(self.cols):
-    
-            self.dfs(self.rows-1,col)
+            self.dfs(self.rows - 1, col)
 
         # LOOP TO CHANGE $ -> O AND O -> X
         for row in range(self.rows):
             for col in range(self.cols):
-                if(self.matrix[row][col]=="$"):
-                    self.matrix[row][col]="O"
-                elif(self.matrix[row][col]=="O"):
-                    self.matrix[row][col]="X"
-                    
+                if self.matrix[row][col] == "$":
+                    self.matrix[row][col] = "O"
+                elif self.matrix[row][col] == "O":
+                    self.matrix[row][col] = "X"
 
     # FUNCTION APPLY DFS ON MATRIX
     def dfs(self, row, col):
 
         # BASE CASE
-        if(row<0 or row==self.rows or col<0 or col==self.cols or self.matrix[row][col]!="O"):
+        if row < 0 or row == self.rows or col < 0 or col == self.cols or self.matrix[row][col] != "O":
             return
 
         # AS THE ELEMENT IS O, CHANGE IT TO $
-        self.matrix[row][col]="$"
+        self.matrix[row][col] = "$"
 
         # MOVE LEFT
-        self.dfs(row,col-1)
+        self.dfs(row, col - 1)
 
         # MOVE UP
-        self.dfs(row-1,col)
+        self.dfs(row - 1, col)
 
         # MOVE RIGHT
-        self.dfs(row,col+1)
+        self.dfs(row, col + 1)
 
         # MOVE DOWN
-        self.dfs(row+1,col)
-    
+        self.dfs(row + 1, col)
+
         return
 
-# CREATE CLASS OBJECT
-board = Board()
-print()
 
-print("Enter X or O as values")
+def main():
+    # INPUTTING TOTAL NUMBER OF ROWS AND COLUMNS
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of columns: "))
 
-# ENTER VALUES IN MATRIX
-board.make()
+    # CREATE CLASS OBJECT
+    board = Board(rows, cols)
+    print()
 
-print("Unsolved board")
+    print("Enter X or O as values")
 
-# SHOWING UNSOLVED BOARD
-board.show()
+    # ENTER VALUES IN MATRIX
+    for i in range(rows):
+        board.matrix.append(input().split(" "))
 
-# SOLVING THE BOARD
-board.solve()
+    # VALIDATING NUMBER OF ROWS OR COLUMNS
+    board.validate_matrix()
 
-print("Solved board")
+    # VALIDATING INPUT
+    board.check()
 
-# DISPLAYING SOLVED BOARD
-board.show()
-        
+    print("Unsolved board")
 
-        
-        
-                        
-                        
-                
+    # SHOWING UNSOLVED BOARD
+    board.show()
 
-        
-                       
+    # SOLVING THE BOARD
+    board.solve()
 
-        
-        
+    print("Solved board")
 
-
-    
-    
+    # DISPLAYING SOLVED BOARD
+    board.show()
 
 
-
-
-                                                                                                                                                                       
-    
-
- 
+if __name__ == "__main__":
+    main()
